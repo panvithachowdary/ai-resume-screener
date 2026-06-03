9596bff5289600214a85fc4f275fa7a731a66a82
panvithachowdary <panvithachowdary@gmail.com>
2026-06-03 23:02:51 +0530

    Allow Vercel frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,10 @@ app=FastAPI(title="AI Resume Screener API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+    "http://localhost:5173",
+    "https://ai-resume-screener-ashen.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
